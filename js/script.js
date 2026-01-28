38b6136cbab7ad1702f663c72e855f92e288e323
sotnik89 <sotnik89@gmail.com>
2026-01-28 13:57:00 +0200

    lesson

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-alert("hello world")
+// var let const
+
+// let var1 = 4;
+// var1 = 5
+// alert(var1);
+
+// var var1 = 4;
+// var1 = 6;
+// alert(var1);
+
+// var undefined / проблема var
+// alert(var1);
+// var var1 = 4;
+// var1 = 6;
+
+// const - тстворюємо таку змінну в яку не можена перезаписати данні
+// const var1 = 7;
+// alert(var1);
+
+// let personName = 9;
+// alert(personName);
+
+// let myNumberVariable = 4;
+// let myFloatVariable = 4.5;
+// let myStringVariable = "Hello!";
+// let myStringVariable2 = 'World!';
+// // alert(myStringVariable2);
+// console.log(4, 4.5, "Ihor", myNumberVariable, myFloatVariable, myStringVariable, myStringVariable2);
+// // console.log(myStringVariable2);
+// let dateFromPrompt = prompt("Hey, please provide your name");
+// console.log(dateFromPrompt)
+// console.log(typeof myFloatVariable)
+
+// PROMPT
+// let userName = prompt("Enter your name");
+// let hello = "Hello, "
+// console.log(hello, userName);
+// console.log("Hello,", userName);
+
+// let userName = prompt("Enter your name");
+// let userAge = prompt("Enter your age")
+// let helloMessage = `Hello, ${userName}. Your age, ${userAge}`;
+// console.log(helloMessage);
+
+// let userName = prompt("Enter your name");
+// let userAge = prompt("Enter your age")
+// console.log(`Hello, ${userName}. Your age, ${userAge}`);
+
+// let a = 4;
+// let b = 5;
+// // let result = a * b + 8 / 2 + a ** b;
+// let result = a % b;
+// console.log(`Result: ${result}`)
+
+// console.log("Floor", Math.floor(67.4));
+// console.log("Ceil",Math.ceil(67.4));
+// console.log("Round", Math.round(67.4));
+// console.log("Trunc", Math.trunc(67.6));
