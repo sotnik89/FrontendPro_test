f2b581fa007486f08f9abb86e4c128683badd5b9
sotnik89 <sotnik89@gmail.com>
2026-01-29 15:28:44 +0200

    ls3-hw3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert("hello world")
+// Розкласти за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл. Приклад:
+//
+// 10369
+//
+// 1 0 3 6 9
+// % / Math.floor
+let divideNumber = 10369;
+
+let d1 = Math.trunc(divideNumber / 10000);
+let d2 = Math.trunc(divideNumber / 10370);
+let d3 = Math.floor(divideNumber / 100) % 100;
+let d4 = Math.floor(divideNumber / 10) % 10;
+let d5 = divideNumber % 10;
+
+let result = `${d1} ${d2} ${d3} ${d4} ${d5}`;
+console.log(result);
