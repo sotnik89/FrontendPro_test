022e3c036887b07c8f6f88d5b4e2be4fb1c43e6e
sotnik89 <sotnik89@gmail.com>
2026-01-29 07:01:09 +0200

    ls3-hw1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("hello world")
+// Створити репозиторій із файлом index.html.
+//     Додати туди невеликий скрипт, мета якого – вивести всі відомі вам типи даних у консоль. Використовуючи оператор typeof та console.log
+
+console.log(typeof 22);
+console.log(typeof "1234");
+console.log(typeof true);
+console.log(typeof null);
