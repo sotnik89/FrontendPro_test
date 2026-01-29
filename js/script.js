cc74a7608a2f10867fea3c731d81f7dbdb060dad
sotnik89 <sotnik89@gmail.com>
2026-01-29 07:19:51 +0200

    ls3-hw2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("hello world")
+// Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (шаблонні рядки);
+
+let userName = prompt("Enter your name");
+let userSureName = prompt("Enter your sureName");
+let userAge = prompt("Enter your age");
+let helloMessage = `Hello, ${userName}. Your age, ${userAge}. Surname, ${userSureName}`;
+console.log(helloMessage);
