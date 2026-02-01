a589de605327d5c96bf315010bc97c35220efeb1
sotnik89 <sotnik89@gmail.com>
2026-02-01 17:01:26 +0200

    ls4-hw4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-alert("hello world")
+// Дано тризначне число, яке надае користувач, потрибно визначити:
+//
+//     Чи правда, що всі цифри однакові?
+//     Чи є серед цифр цифри однакові?
+
+// let threeNumber = +prompt("Number"); - змінити тип на number
+
+// let threeNumber = prompt("Number");
+// let threeDigit = +threeNumber; - змінити тип на number
+
+let threeNumber = Number(prompt("Number"));
+console.log(typeof threeNumber)
+if (!threeNumber || isNaN(threeNumber)) {
+    console.log("false date")
+} else {
+    let d1 = Math.trunc(threeNumber / 100);
+    let d2 = Math.trunc(threeNumber / 10) % 10;
+    let d3 = threeNumber % 10;
+
+    if (d1 === d2 === d3) {
+        console.log("Same")
+    } else {"Not same"}
+    if (d1 === d2 || d1 === d3 || d2 === d3){
+        console.log("Some numbers same");
+    } else {
+        console.log("Not same");
+    }
+}
