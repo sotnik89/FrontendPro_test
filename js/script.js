caf8e82068721fb055d9c89a85eb1d49a8675286
sotnik89 <sotnik89@gmail.com>
2026-02-01 16:00:47 +0200

    username

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-alert("hello world")
+// За допомогою prompt запитати “ім'я користувача”.
+// За допомогою alert вивести "Hello, John! How are you?" , де “John” це те, що ввів користувач
+
+let userName = prompt("User Name");
+alert(`Hello, ${userName}.! How are you?`)
