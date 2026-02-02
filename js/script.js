ca35ece9bbcb8b92c1bf7de0acd7fc9b82f74aa3
sotnik89 <sotnik89@gmail.com>
2026-02-02 17:52:45 +0200

    template

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert("hello world")
+let numOrStr = prompt("input number or string")
+// console.log(numOrStr);
+
+// if (numOrStr === null){
+//     console.log("ви скасували")
+// } else if (numOrStr.trim() === "") {
+//     console.log("Empty string");
+// } else if (isNaN(+numOrStr)){
+//     console.log(" Number is Ba_NaN");
+// } else{
+//     console.log("OK!");
+// }
+
+switch (numOrStr) {
+    case null:
+        console.log("ви скасували")
+        break;
+    case '':
+        console.log("Empty string")
+        break;
+    case isNaN(+numOrStr):
+        console.log(" Number is Ba_NaN")
+        break;
+    default: console.log("OK!");
+}
