739ca4bd876e7a7e091d33b7a3e6340d348bbf04
sotnik89 <sotnik89@gmail.com>
2026-02-04 14:11:52 +0200

    homework2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("hello world")
+// Один долар коштує 26 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
+// const kurs = 26;
+for(let i = 10; i <= 100; i += 10){
+    let hrn = i * 26;
+    // let hrn = i * kurs;
+    console.log(`${i} USD = ${hrn} UAH`)
+};
