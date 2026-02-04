7f0ff911c977eba37886dad58265ee761f8c07a9
sotnik89 <sotnik89@gmail.com>
2026-02-04 14:49:54 +0200

    homework3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("hello world")
+// Дано ціле число N (ввести через prompt). Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N
+
+// continue break
+let figure = +prompt("Enter your number");
+
+for (let i = 1; i <= 100; i++ ){
+    if(i * i <= figure){
+        console.log(i);
+    } else {
+        break;
+    }
+}
