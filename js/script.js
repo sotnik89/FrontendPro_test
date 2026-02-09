f718b3115c4822078bc21f2df2ff38015ab9fd2d
sotnik89 <sotnik89@gmail.com>
2026-02-09 15:28:29 +0200

    ls6_hw1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-alert("hello world")
+// Створіть об'єкт, що містить інформацію про користувача, таку як ім'я, вік, місце проживання тощо. Створіть метод об'єкту для отримання та відображення цих даних.
+const user = {
+    name: "John",
+    age: 25,
+    address: {
+        city: "Odesa",
+        street: "Glushko"
+    }
+}
+console.log(Object.values(user));
+console.log(Object.keys(user));
+
+for (const key of Object.keys(user)){
+    console.log(key)
+};
+for (const key of Object.keys(user)){
+    console.log(user[key])
+};
+
+for (const key of Object.keys(user)){
+    console.log(key, ":", user[key])
+};
+
+
