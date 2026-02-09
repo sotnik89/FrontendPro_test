5287ddbad22197b5e770bb3ece74fd09d871f341
sotnik89 <sotnik89@gmail.com>
2026-02-09 17:17:38 +0200

    ls6_hw2

--- js/script.js
+++ js/script.js
@@ -2,22 +2,21 @@
 
 const numbers = [1, 5, 6, 8, 11, 18, 20, 22, 27];
 const evenNumbers = [];
-// for(const number of numbers){
-//
-//
-// }
-for(let i = 0; i < numbers.length; i++){
-    if(numbers[i] % 2 === 0){
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
         evenNumbers.push(numbers[i]);
-        console.log(evenNumbers)
     }
-
-    for (const number of numbers){
-
-    }
-
-    // } else {
-    //     console.log("This is not even number")
-    // }
-
 }
+console.log(evenNumbers);
+
+
+// let i = 0;
+// i++;
+// for (const i of numbers){
+//     if (numbers[i] % 2 === 0) {
+//         evenNumbers.push(numbers[i]);
+//     }
+// }
+// console.log(evenNumbers);
+
