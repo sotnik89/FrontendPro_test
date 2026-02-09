2dedb0c1429ad1163495ec09531166f09830e6d2
sotnik89 <sotnik89@gmail.com>
2026-02-09 16:58:38 +0200

    not final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("hello world")
+// Маєте масив чисел. Використовуйте вже існуючі методи масиву для створення нового масиву, в якому лише парні числа з оригінального масиву.
+
+const numbers = [1, 5, 6, 8, 11, 18, 20, 22, 27];
+const evenNumbers = [];
+// for(const number of numbers){
+//
+//
+// }
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 === 0){
+        evenNumbers.push(numbers[i]);
+        console.log(evenNumbers)
+    }
+
+    for (const number of numbers){
+
+    }
+
+    // } else {
+    //     console.log("This is not even number")
+    // }
+
+}
