82a548c9dc352342eeacb92fed7e4c31ebb66ec6
sotnik89 <sotnik89@gmail.com>
2026-02-13 13:30:46 +0200

    ready_ls7_hw3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("hello world")
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+//
+//     Наприклад:
+//
+// const array = [1, 3, 4, 6, 2, 5, 7];
+//
+// removeElement(array,4);
+//
+// console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
+
+const firstArray = [1, 3, 4, 6, 2, 5, 7];
+
+function removeElement(array, item) {
+    return firstArray.filter(function (firstArrayI) {
+            return firstArrayI !== item ? true : false
+        }
+    );
+    // number => number !== item);
+}
+const result = removeElement(firstArray, 4);
+console.log(result);
