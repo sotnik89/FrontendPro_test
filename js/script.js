f28d913e9dfa9622e6882d0972cad001182bb0f9
sotnik89 <sotnik89@gmail.com>
2026-02-17 19:11:55 +0200

    ls8_hw1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("hello world")
+// Написати функцію, яка приймає 1 параметр. Та скадае значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+//
+// console.log(sum(4)); // 4
+// console.log(sum(6)); // 10
+// console.log(sum(10)); // 20
+// console.log(sum(7)); // 27
+
+function outer(){
+    let count = 0;
+
+    return function (item){
+        count += item;
+        return count;
+    }
+}
+const sum = outer();
+
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7));
