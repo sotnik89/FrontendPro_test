24f23dfae024f4adc2e749e1241ee8a42a453238
sotnik89 <sotnik89@gmail.com>
2026-02-20 09:56:46 +0200

    homework 2002

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert("hello world")
+// Створити функцію для розрахунку добутку двох чисел, що викликається так: name(5)(2). Функція повинна повертати результат (у середині функції не має бути консоль лога!)
+
+const multiply1 = a => b => a * b;
+const result1 = multiply1(5)(2)
+console.log(result1)
+
+function multiply (first){
+    return function (second){
+        return first * second
+    }
+}
+const result = multiply(11)(1254)
+console.log(result)
+
+const multi = a => b => a * b;
+const result2 = multi(4)(54)
+console.log(result2)
