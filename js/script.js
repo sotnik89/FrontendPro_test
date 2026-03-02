81bde5fc3cf0a7e3aefaeb69e658c11c6bcf5cc7
sotnik89 <sotnik89@gmail.com>
2026-03-02 14:54:17 +0200

    homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("hello world")
+// Є блок із текстом на сторінці та кнопка. При натисканні на кнопку текст змінює колір. При повторному натисканні – повертається попередній колір
+
+const myButton = document.querySelector(".myBtn");
+const textX = document.querySelector(".text")
+
+function changeColor(){
+    textX.classList.toggle("textColorChange")
+}
+myButton.addEventListener("click", changeColor);
+
+
+
