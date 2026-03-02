a82c9216ccb7699662c383e5df0abf80c379fdb2
sotnik89 <sotnik89@gmail.com>
2026-03-02 17:27:38 +0200

    homework0203

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert("hello world")
+// На сторінці є дві кнопки. При натисканні на першу кнопку користувач повинен ввести в prompt посилання, при натисканні на другу – переадресовується на інший сайт (за раніше введеним посиланням).
+
+const promptLink = document.querySelector("#linkFirst");
+const enterLink = document.querySelector("#linkSecond");
+
+let url = "https://vinfcup.com.ua/"
+promptLink.addEventListener("click", () => {
+    window.location.href = (prompt("Enter link"))
+
+});
+
+enterLink.addEventListener("click", () => {
+    window.open("https://vinfcup.com.ua/", "_blank")
+})
+//
+// let link = "";
+// window.addEventListener("click", (e) => {
+//         if (e.target.id === "linkFirst") {
+//             link = prompt("Enter link");
+//         }
+//         if (e.target.id === "linkSecond") {
+//             link ? location.href = link : alert("Please enter link")
+//         }
+//     }
+// )
