b0eadfa9baed309536cef0de93b9eda8c46f0b97
sotnik89 <sotnik89@gmail.com>
2026-03-03 17:05:50 +0200

    homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-alert("hello world")
+// Створіть HTML-сторінку, яка містить список завдань (to-do list) з можливістю додавання нових завдань. Ваше ціль - використовуючи делегування подій, створити обробник подій для списку завдань, який дозволить видаляти завдання при кліку на них.
+//
+//     Покроковий план:
+//
+//     Створіть HTML-елементи: список завдань ul, текстове поле для вводу нових завдань та кнопку для додавання.
+//     Додайте обробник подій до списку завдань ul, використовуючи делегування.
+//     При кліку на будь-якій кнопці видалення, видаліть цей пункт.
+//     Додайте можливість вводити нові завдання у текстове поле і додавати їх до списку за допомогою кнопки.
+
+const tasks = document.querySelector("#listOfTask");
+const input = document.querySelector("#inputT")
+const btn = document.querySelector("#addTask");
+
+tasks.addEventListener("click", (e) => {
+    if(e.target.classList.contains("deleteBtn")){
+        e.target.parentElement.remove();
+        return
+    }
+})
+input.addEventListener("change", (e) => {
+    console.log(e.target.value)
+})
+
+btn.addEventListener("click", (e) => {
+    if (input.value.trim()){
+        tasks.innerHTML += `<li>${input.value} <button class="deleteBtn">Видалити`
+        input.value = "";
+    }
+})
