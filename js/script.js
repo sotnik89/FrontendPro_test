47c01df0d6933cbb92cc248bd9e708cd3d3c8d9e
sotnik89 <sotnik89@gmail.com>
2026-03-04 12:13:36 +0200

    lesson13

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("hello world")
+const agreeCheckBox = document.querySelector("#agreeCheckBox")
+const citySelect = document.querySelector("#citySelect");
+
+
+agreeCheckBox.addEventListener("change", function () {
+    console.log("Checkbox changed.", this.checked)
+})
+citySelect.addEventListener("change", function (){
+    console.log("Selected", this.value)
+})
