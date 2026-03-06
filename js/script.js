53619c6fe25910f86c5d8a6c81afad91a3701698
sotnik89 <sotnik89@gmail.com>
2026-03-06 16:52:08 +0200

    not write error

--- js/script.js
+++ js/script.js
@@ -9,34 +9,65 @@
 // Після відправки, в консоль відображаємо дані, які ввів користувач.
 //
 //     Під час помилки показувати її під полем.
-const formLogin = document.querySelector("#formLogin")
-const nameInput = document.querySelector("#nameInput")
-const messageText = document.querySelector("#messageText")
-const email = document.querySelector("#email")
-const phone = document.querySelector("#phone")
-const errorPhone = document.querySelector("#errorPhone");
-const errorEmail = document.querySelector("#errorEmail")
-const btn = document.querySelector("#btn")
 
-btn.addEventListener("click", function (){
-    console.log(
-        formLogin.checkValidity(),
-        formLogin.reportValidity()
-    );
-    formLogin.reset();
-})
-formLogin.addEventListener("submit", function (e){
-    e.preventDefault()
-    const formData = new formData(formLogin);
-    const phone = formData.get("phone")
-    const email = formData.get("email");
-    if(!email || !email.trim() || !email.includes("@")){
-        errorEmail.style.display = "inline"
-    }
-    if(!phone || !phone.trim() || !phone.includes("+380")){
-        errorPhone.style.display = "inline"
-    }
-    this.reset();
-})
+const errorEmail = document.querySelector("#errorEmail")
+
+
+const emailAddress = "";
+const emailRegex = /.+@.+\..+/;
+if (emailRegex.test(emailAddress)) {
+        console.log("Email адреса правильна.");
+} else {
+    errorEmail.style.display = "inline"
+    console.log("Неправильний формат email адреси.");
+}
+console.log(emailAddress)
+
+const phoneNumber = phone.value.trim().match(/^\+380\d{9}$/);
+if(emailRegex.test(phoneNumber)){
+    console.log("Phone is correct");
+} else {
+    console.log("Not corrected phone number");
+}
+console.log(phoneNumber)
+
+
+
+
+
+
+
+
+// email.addEventListener("input", function (e){
+//     const email = e.target.value;
+//     if(email.includes("@")){
+//         btn.innerText = ""
+//     } else {
+//         btn.innerText = "Email should have '@'"
+//     }
+// console.log(btn)
+// })
+
+// btn.addEventListener("click", function (){
+//     console.log(
+//         formLogin.checkValidity(),
+//         formLogin.reportValidity()
+//     );
+//     formLogin.reset();
+// })
+// formLogin.addEventListener("submit", function (e){
+//     e.preventDefault()
+//     const formData = new formData(formLogin);
+//     const phone = formData.get("phone")
+//     const email = formData.get("email");
+//     if(!email || !email.trim() || !email.includes("@")){
+//         errorEmail.style.display = "inline"
+//     }
+//     if(!phone || !phone.trim() || !phone.includes("+380")){
+//         errorPhone.style.display = "inline"
+//     }
+//     this.reset();
+// })
+
 
 
