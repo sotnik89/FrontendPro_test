9a465bb95bc08719f9197bcdac8b448b165e4734
sotnik89 <sotnik89@gmail.com>
2026-03-12 13:23:11 +0200

    1203

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Доробити TODO лист, в якому буде можливість:
+//
+//     Додати завдання
+// Видалити завдання
+// Відзначити як виконану
+// Усі дані повинні зберегтися після перезавантаження сторінки.
+const input = document.querySelector(".js--form__input");
+const jsForm = document.querySelector(".js--form");
+const todosWrapperList = document.querySelector(".js--todos-wrapper");
+
+
