4c1a8aef8b50e528500712aabad88f7fa0291b5c
sotnik89 <sotnik89@gmail.com>
2026-03-13 15:31:39 +0200

    Pythagoras_table

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+// Вивести таблицю Піфагора (10×10), таблиця повинна бути створена динамічно
+function generateTable(size) {
+    const container = document.querySelector('#table-box');
+    const table = document.createElement('table');
 
+    for (let i = 1; i <= size; i++) {
+        const rowTr = document.createElement('tr');
+
+        for (let j = 1; j <= size; j++) {
+            const cellTd = document.createElement('td');
+            cellTd.textContent = i * j;
+            if (i === j && i > 1) {
+                cellTd.classList.add('diagonal');
+            }
+            rowTr.appendChild(cellTd);
+        }
+        table.appendChild(rowTr);
+    }
+    container.appendChild(table);
+}
+generateTable(10);
