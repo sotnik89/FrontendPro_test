b85489dda3fcf76b4e74cc108d094966275d2dd0
sotnik89 <sotnik89@gmail.com>
2026-03-18 09:05:51 +0200

    ls17_hw3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+// Створіть клас BankAccount, який буде представляти банківський рахунок. Додайте властивість балансу та методи для внесення та зняття грошей.
 
+class BankAccount {
+    constructor(balance) {
+        this.balance = balance;
+    }
+    getBalance() {
+        console.log(`Current balance: ${this.balance}`)
+    }
+    deposit (amount)
+    {
+        this.balance += amount
+    }
+    withdraw (amount)
+    {
+        if (amount > this.balance) {
+            console.log("Not enough money")
+            return;
+        }
+        this.balance -= amount;
+    }
+}
+
+const account1 = new BankAccount(1000);
+
+account1.getBalance();
+account1.deposit(500);
+account1.getBalance();
+account1.withdraw(200);
+account1.getBalance();
 
 
 
