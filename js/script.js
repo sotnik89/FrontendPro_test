d164cfc9a44ae1f34d6ade66e619bcaa5b47e638
sotnik89 <sotnik89@gmail.com>
2026-03-18 10:03:36 +0200

    ls17_hw1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+// Створіть клас Calculator, який буде мати методи для виконання базових математичних операцій: додавання, віднімання, множення та ділення.
 
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }divide(a, b) {
+        if(b === 0){
+            return ("Can not divide by 0")
+        }
+        return a / b;
+    }
+}
+
+const calc = new Calculator();
+
+console.log(calc.add(5, 3));
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 6));
+console.log(calc.divide(8, 2));
