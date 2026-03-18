a0a332020813ee8f7acb69f2b7476a7952be6c7d
sotnik89 <sotnik89@gmail.com>
2026-03-18 09:50:10 +0200

    ls17_hw2

--- js/script.js
+++ js/script.js
@@ -98,7 +98,23 @@ account1.getBalance();
 account1.withdraw(200);
 account1.getBalance();
 
+//* ls17_hw2
+class Coach {
+    constructor(name, specialization, rating) {
+        this.name = name;
+        this.specialization = specialization;
+        this.rating = rating;
+    }
+    displayInfo(){
+        console.log(`"Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}"`)
+    }
+}
 
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+
+coach1.displayInfo(); // "Coach: John Doe, Specialization: Fitness, Rating: 4.7"
+coach2.displayInfo(); // "Coach: Alice Smith, Specialization: Yoga, Rating: 4.9"
 
 
 
@@ -198,3 +214,5 @@ account1.getBalance();
 // car1.showInfo();
 // car2.showInfo();
 // console.log(car1.showInfo === car2.showInfo)
+
+
