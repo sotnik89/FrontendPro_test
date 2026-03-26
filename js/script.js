f459756a5c14e4b888323e0cc49a942b4772ef76
sotnik89 <sotnik89@gmail.com>
2026-03-26 15:13:19 +0200

    ls19_hw1

--- js/script.js
+++ js/script.js
@@ -1,105 +1,37 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function User(name, balance){
-//     this.name = name;
-//     this.balance = balance;
-//     this.debit = function (amount){
-//         this.balance = this.balance += amount;
-//     }
-//     this.showBalance = function (){
-//         console.log(`${name} current balance: ${this.balance} USD`)
-//     }
-//     this.withdraw = function (amount){
-//         if(amount > this.balance){
-//             console.log("Not enough")
-//             return;
-//         }
-//         this.balance -= amount
-//     }
-// }
+// За допомогою запиту вивести виджет погоди. Ресурс API https://openweathermap.org/current
 //
-// function Admin(name, balance, role) {
-//     User.call(this, name, balance);
-//     this.role = role;
-//     this.showRole = function (){
-//         console.log("You are:", this.role)
-//     }
-//     this.showBalance = function (){
-//         console.log("Admin balance:", this.balance, "USD");
-//     }
-// }
-//
-// const user1 = new User ("Alex", 100);
-// const user2 = new User ("Bob", 45);
-// const userAdmin = new Admin ("Alice", 99, "superadmin")
-//
-// user1.showBalance();
-// user2.showBalance();
-// user1.withdraw(90)
-// user1.showBalance()
-// userAdmin.showBalance()
-//
-// user1.debit(20);
-// user2.debit(60);
-// userAdmin.debit(33);
-//
-// user1.showBalance();
-// user2.showBalance();
-// userAdmin.showBalance()
-//
-// function Car(brand, speed) {
-//     this.brand = brand;
-//     this.speed = speed;
-//
-//     // this.showInfo = function (){
-//     //     console.log("Brand:", this.brand, " | ", this.speed, "miles/hour" )
-//     // }
-// }
-//
-// Car.prototype.showInfo = function () {
-//     console.log("Brand:", this.brand, " | ", this.speed, "miles/hour")
-// }
-// Car.prototype.accelerate = function (value) {
-//     this.speed += value;
-// }
-//
-// const car1 = new Car("BMW", 70);
-// const car2 = new Car("VW", 100)
-//
-// car1.showInfo();
-// car2.showInfo();
-// car1.accelerate(10)
-// car2.accelerate(20)
-// car1.showInfo();
-// car2.showInfo();
-// console.log(car1.showInfo === car2.showInfo)
+//     Також потрібно додати кнопку оновлення данних.
+
+const url =     `https://api.openweathermap.org/data/2.5/weather?lat=46.48&lon=30.73&appid=26567086ca3cc96a017379cca2bb9c04&units=metric`;
+
+const refreshBtn = document.querySelector("#refresh-btn");
+const desc = document.querySelector("#desc");
+const city = document.querySelector("#city");
+const temp = document.querySelector("#temp");
+const humidity = document.querySelector("#humidity");
+
+function renderWeather(data) {
+    city.textContent = data.name;
+    temp.innerHTML = `
+    Temperature: ${Math.round(data.main.temp)}°C <br>
+    Feels like: ${Math.round(data.main.feels_like)}°C
+    `;
+    desc.textContent = data.weather[0].description;
+    humidity.textContent = `Humidity: ${data.main.humidity} %`;
+}
+function getWeather() {
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            renderWeather(data);
+            console.log("Data updated", data);
+        })
+        .catch(err => {
+            console.error(err);
+            desc.textContent = "Error connection";
+        });
+}
+refreshBtn.addEventListener("click", getWeather);
+getWeather();
