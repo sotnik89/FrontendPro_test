8dd1c174bfcbf7f377bbec1fa3d1c7607a73c6c7
sotnik89 <sotnik89@gmail.com>
2026-03-27 15:53:46 +0200

    timer

--- js/script.js
+++ js/script.js
@@ -1,105 +1,29 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function User(name, balance){
-//     this.name = name;
-//     this.balance = balance;
-//     this.debit = function (amount){
-//         this.balance = this.balance += amount;
-//     }
-//     this.showBalance = function (){
-//         console.log(`${name} current balance: ${this.balance} USD`)
-//     }
-//     this.withdraw = function (amount){
-//         if(amount > this.balance){
-//             console.log("Not enough")
-//             return;
-//         }
-//         this.balance -= amount
-//     }
-// }
+// Реалізувати таймер відліку:
 //
-// function Admin(name, balance, role) {
-//     User.call(this, name, balance);
-//     this.role = role;
-//     this.showRole = function (){
-//         console.log("You are:", this.role)
-//     }
-//     this.showBalance = function (){
-//         console.log("Admin balance:", this.balance, "USD");
-//     }
-// }
-//
-// const user1 = new User ("Alex", 100);
-// const user2 = new User ("Bob", 45);
-// const userAdmin = new Admin ("Alice", 99, "superadmin")
-//
-// user1.showBalance();
-// user2.showBalance();
-// user1.withdraw(90)
-// user1.showBalance()
-// userAdmin.showBalance()
-//
-// user1.debit(20);
-// user2.debit(60);
-// userAdmin.debit(33);
-//
-// user1.showBalance();
-// user2.showBalance();
-// userAdmin.showBalance()
-//
-// function Car(brand, speed) {
-//     this.brand = brand;
-//     this.speed = speed;
-//
-//     // this.showInfo = function (){
-//     //     console.log("Brand:", this.brand, " | ", this.speed, "miles/hour" )
-//     // }
-// }
-//
-// Car.prototype.showInfo = function () {
-//     console.log("Brand:", this.brand, " | ", this.speed, "miles/hour")
-// }
-// Car.prototype.accelerate = function (value) {
-//     this.speed += value;
-// }
-//
-// const car1 = new Car("BMW", 70);
-// const car2 = new Car("VW", 100)
-//
-// car1.showInfo();
-// car2.showInfo();
-// car1.accelerate(10)
-// car2.accelerate(20)
-// car1.showInfo();
-// car2.showInfo();
-// console.log(car1.showInfo === car2.showInfo)
+//     Початок таймера визначати із змінної
+// Відобразити на сторінці час у форматі 01:25
+// Коли закінчився таймер зупинити його
+const displayTimer = document.querySelector(".displayTimer")
+const timer_stopped = document.querySelector(".timer_stopped")
+let timer = 90;
+
+function formatTimer (value){
+    let minutes = Math.floor(value / 60);
+    let seconds = value % 60;
+    const showMinutes = String(minutes).padStart(2, '0');
+    const showSeconds = String(seconds).padStart(2, '0');
+    return `${showMinutes}:${showSeconds}`;
+};
+
+const countDown = setInterval(() => {
+    displayTimer.innerHTML = `Сountdown timer:<br> ${formatTimer(timer)}`;
+    if (timer <= 0) {
+        clearInterval(countDown);
+        timer_stopped.textContent = "Timer stopped";
+        timer_stopped.classList.add('timer_stopped');
+    } else {
+        timer--;
+    }
+}, 1000);
+
+
